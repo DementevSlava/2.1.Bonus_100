d9477fe8c0c4d7c9fa86c9cb0d249bc519d239e2
Dementev Slava <kursant344@gmail.com>
2020-03-26 15:43:52 +0300

    Fix //

--- Bonus.java
+++ Bonus.java
@@ -1,18 +1,18 @@
-package Blok_3_TipyDannyh.Z2_Bonus_100;
+// package Blok_3_TipyDannyh.Z2_Bonus_100; //Заменить на свой (Для IDEA)
 
-import java.util.Locale;
+import java.util.Locale;  //Использовать точки, а не запятые
 import java.util.Scanner;
 
 public class Bonus {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
         double cash = 101.56; //Баланс в рублях
-        int re_cash = (int) cash * 100;
+        int re_cash = (int) cash * 100;//Перевод в копейки и целые числа
         System.out.println("Ведите сумму перевода");
         double transferAmount = scanner.nextDouble(); //Ввод суммы
-        int re_transferAmount = (int) transferAmount * 100;
+        int re_transferAmount = (int) transferAmount * 100; //Перевод в копейки и целые числа
         int bonus = re_transferAmount / 100 / 100; // Делим на 100 - перевод в рубли и демим на 100 бонус за каждые 100 руб.
-        if (transferAmount > 1000) { // eсли клиент пополняет счёт более чем на 1000 рублей
+        if (transferAmount > 1000) { // Если клиент пополняет счёт более чем на 1000 рублей (Условие)
             System.out.println("Бонус за перевод составляет: " + bonus + " Руб.");
             System.out.println("Сумма на счету " + (cash + transferAmount + bonus) + " Руб.");
         } else {
